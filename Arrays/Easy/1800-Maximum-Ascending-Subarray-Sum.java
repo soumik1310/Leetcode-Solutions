452b9aee38a2bd51f6488d77a19f33439aa96550
soumik1310 <soumiksarkarrr@gmail.com>
2026-06-14 20:48:49 +0530

    Create 1800-Maximum-Ascending-Subarray-Sum.java

---
+++ Arrays/Easy/1800-Maximum-Ascending-Subarray-Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+
+        int currSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] > nums[i - 1]) {
+                currSum += nums[i];
+            } else {
+                currSum = nums[i];
+            }
+
+            maxSum = Math.max(maxSum, currSum);
+        }
+
+        return maxSum;
+    }
+}
